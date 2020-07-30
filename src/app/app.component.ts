6b4ebce1ad86ec91a901f933f983beebfc695919
Kanika1304 <kanikagupta1304@gmail.com>
2020-07-30 15:40:05 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  title = 'To-Do-List';
+  t=[
+    {
+      label: 'Milk',
+      status: true,
+      quantity: 3
+    },
+    {
+      label: 'Veggies',
+      status: false,
+      quantity: 8
+    },
+    {
+      label: 'Snacks',
+      status: false,
+      quantity: 1
+    },
+    {
+      label: 'butter',
+      status: true,
+      quantity: 1
+    },
+
+  ];
+
+  addTodo(newTextLabel){
+    var newText={
+      label: newTextLabel,
+      status: false,
+      quantity: 0
+    };
+
+    if(newText.label == ''){
+    }
+    else{
+      this.t.push(newText);
+    }
+  }
+
+  delTodo(t1){
+    this.t= this.t.filter( ting => ting.label !== t1.label);
+  }
 }
